6b2cd11f12077ba00853c6fa9398795ec1f7ce9a
sioremon <t17651sn@sfc.keio.ac.jp>
2020-12-25 02:01:08 +0900

    Improve:Support for reinstallation

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-echo 'Making a Directory in /opt. Please Enter your password!'
-sudo mkdir /opt/shell
-sudo cp ./cnvexif.sh /opt/shell
-echo 'PATH="$PATH:/opt/shell"' >> ~/.bashrc
-echo "alias cnvexif='cnvexif.sh'">> ~/.bashrc 
-source ~/.bashrc
+declare ret
+# check existence directory
+if [[ ! -d /opt/cnvexif ]]; then
+    echo 'Making a Directory in /opt. Please Enter your password!'
+    sudo mkdir /opt/cnvexif
+    sudo cp ./cnvexif.sh /opt/cnvexif
+fi
+# check existence .sh file
+if [[ -d /opt/cnvexif ]]; then
+    if [[ ! -f /opt/cnvexif/cnvexif.sh ]]; then
+        sudo cp ./cnvexif.sh /opt/cnvexif
+    fi
+fi
+
+grep 'PATH="$PATH:/opt/cnvexif"' ~/.bashrc 
+ret=$?
+if [ ! $ret -eq 0 ]; then
+    echo 'PATH="$PATH:/opt/cnvexif"' >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+grep -E "alias\s+cnvexif='cnvexif.sh'" ~/.bashrc 
+ret=$?
+if [ ! $ret -eq 0 ]; then
+    echo "alias cnvexif='cnvexif.sh'">> ~/.bashrc 
+    source ~/.bashrc
+fi
